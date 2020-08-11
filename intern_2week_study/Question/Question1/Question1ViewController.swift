e2a76b34c54a9acd85aeca9853e7e02c40725e21
taigaozawa <taigaozawa@gmail.com>
2020-08-11 20:30:23 +0900

    課題1-1を実装

--- intern_2week_study/Question/Question1/Question1ViewController.swift
+++ intern_2week_study/Question/Question1/Question1ViewController.swift
@@ -2,11 +2,38 @@ import UIKit
 
 final class Question1ViewController: UIViewController {
     
+    // MARK: Properties
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var addTextButton: UIButton!
     @IBOutlet weak var textView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // 起動時に textView を空にする
+        textView.text = ""
+        textField.delegate = self
     }
+    @IBAction func addText(_ sender: UIButton) {
+        self.textFieldDidEndEditing(textField)
+    }
+    
+}
+
+// MARK: UITextFieldDelegate
+extension Question1ViewController: UITextFieldDelegate {
+    // textField で Return されたときに呼び出されるメソッド
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // キーボードを隠す
+        textField.resignFirstResponder() // FirstResponder を解除
+        return true
+    }
+    // FirstResponder が解除された後に呼び出されるメソッド
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        // textField に入力されたテキストを textView に改行して表示
+        if let newText = textField.text {
+            textView.text += newText + "\n" // 改行して追加
+            textField.text = "" // 入力するごとに textField を空にする
+        }
+    }
+    
 }
