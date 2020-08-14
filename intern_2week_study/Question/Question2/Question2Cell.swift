12b64dd4310f6b30b89bf24541a87f9f2dcd65ec
taigaozawa <taigaozawa@gmail.com>
2020-08-14 20:07:52 +0900

    課題2-2: areaImagesの内容をcellのImageViewに表示

--- intern_2week_study/Question/Question2/Question2Cell.swift
+++ intern_2week_study/Question/Question2/Question2Cell.swift
@@ -1,10 +1,13 @@
+import Nuke
 import UIKit
 
 class Question2Cell: UITableViewCell {
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var areaImageView: UIImageView!
     
-    public func setCellImageAndLabel (labelText: String) {
+    // MARK: Methods
+    public func setCellImageAndLabel (labelText: String, imageURL: URL) {
         label.text = labelText
+        Nuke.loadImage(with: imageURL, into: areaImageView)
     }
 }
--- intern_2week_study/Question/Question2/Question2ViewController.swift
+++ intern_2week_study/Question/Question2/Question2ViewController.swift
@@ -6,6 +6,16 @@ final class Question2ViewController: UIViewController {
     
     private let areaTexts: [String] = ["茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県"]
     
+    private let areaImages: [String] = [
+        "https://uub.jp/47/ibaraki/ibaraki_kensho.png",
+        "https://uub.jp/47/tochigi/tochigi_kensho.png",
+        "https://uub.jp/47/gunma/gunma_monsho.png",
+        "https://uub.jp/47/saitama/saitama_kensho.png",
+        "https://uub.jp/47/chiba/chiba_kensho.png",
+        "https://uub.jp/47/tokyo/tokyo_symbol.png",
+        "https://uub.jp/47/kanagawa/kanagawa_kensho.png"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,12 +32,19 @@ extension Question2ViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "question2Cell", for: indexPath) as? Question2Cell else {
+        guard areaTexts.count == areaImages.count else {
+            print("areaTexts と areaImages の要素数が異なります。")
             return UITableViewCell()
         }
         
-        cell.setCellImageAndLabel(labelText: areaTexts[indexPath.row])
+        let areaText = areaTexts[indexPath.row]
+        let areaImage = areaImages[indexPath.row]
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "question2Cell", for: indexPath) as? Question2Cell, let areaImageURL = URL(string: areaImage) else {
+            return UITableViewCell()
+        }
+        
+        cell.setCellImageAndLabel(labelText: areaText, imageURL: areaImageURL)
         
         return cell
     }
