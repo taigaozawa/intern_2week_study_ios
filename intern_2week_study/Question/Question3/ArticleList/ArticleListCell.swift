9cb52cfb92347a17c0eef10b496dd2e20fb35836
taigaozawa <taigaozawa@gmail.com>
2020-08-16 02:27:44 +0900

    tableViewを実装

--- intern_2week_study/Question/Question3/ArticleList/ArticleListCell.swift
+++ intern_2week_study/Question/Question3/ArticleList/ArticleListCell.swift
@@ -6,13 +6,23 @@
 //  Copyright © 2020 caraquri. All rights reserved.
 //
 
+import Nuke
 import UIKit
 
 class ArticleListCell: UITableViewCell {
-
+    
+    // MARK: Properties
+ 
+    @IBOutlet weak var cellImageView: UIImageView!
+    @IBOutlet weak var cellLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+    }
+    
+    func setCellImageAndLabel(labelText: String, imageURL: URL) {
+        cellLabel.text = labelText
+        Nuke.loadImage(with: imageURL, into: cellImageView)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- intern_2week_study/Question/Question3/ArticleList/ArticleListViewController.swift
+++ intern_2week_study/Question/Question3/ArticleList/ArticleListViewController.swift
@@ -11,21 +11,35 @@ import UIKit
 class ArticleListViewController: UIViewController {
     
     // MARK: Properties
-    @IBOutlet weak var articleListTableView: UITableView!
+    @IBOutlet weak var tableView: UITableView!
     
-    private let areaTexts: [String] = ["茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県"]
+    private let articleTitles: [String] = ["茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県"]
+    private let articleImages: [String] = [
+        "https://uub.jp/47/ibaraki/ibaraki_kensho.png",
+        "https://uub.jp/47/ibaraki/ibaraki_kensho.png",
+        "https://uub.jp/47/ibaraki/ibaraki_kensho.png",
+        "https://uub.jp/47/ibaraki/ibaraki_kensho.png",
+        "https://uub.jp/47/ibaraki/ibaraki_kensho.png",
+        "https://uub.jp/47/ibaraki/ibaraki_kensho.png",
+        "https://uub.jp/47/ibaraki/ibaraki_kensho.png"
+    ]
     
     var sentKeyWord: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // tableView の設定
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UINib(nibName: "ArticleListCell", bundle: nil), forCellReuseIdentifier: "articleListCell")
+        tableView.rowHeight = 50
+        
+        // API をコールする
         guard let keyWord = sentKeyWord else {
             return
         }
-        
         print("キーワード「" + keyWord + "」を受け取りました")
-        
         callQiitaAPI(keyWord: keyWord)
     }
     
@@ -47,4 +61,33 @@ class ArticleListViewController: UIViewController {
         print(keyWord + " を検索します。")
     }
 
+}
+
+extension ArticleListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articleTitles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard articleTitles.count == articleImages.count else {
+            return UITableViewCell()
+        }
+        
+        let articleTitle = articleTitles[indexPath.row]
+        let articleImage = articleImages[indexPath.row]
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "articleListCell", for: indexPath) as? ArticleListCell, let articleImageURL = URL(string: articleImage) else {
+            return UITableViewCell()
+        }
+        
+        cell.setCellImageAndLabel(labelText: articleTitle, imageURL: articleImageURL)
+        
+        return cell
+    }
+}
+
+extension ArticleListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Hello")
+    }
 }
